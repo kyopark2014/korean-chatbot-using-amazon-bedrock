2e10ec87fc14de9e9b84f58f222d382e73641afe
Kyoungsu Park <kyopark2014@gmail.com>
2024-07-05 23:55:45 +0900

    update

--- cdk-korean-chatbot/lib/cdk-korean-chatbot-stack.ts
+++ cdk-korean-chatbot/lib/cdk-korean-chatbot-stack.ts
@@ -940,6 +940,19 @@ export class CdkKoreanChatbotStack extends cdk.Stack {
     lambdaS3event.addEventSource(s3PutEventSource); */
 
     // S3 - Lambda(S3 event) - SQS(fifo) - Lambda(document)
+    // DLQ
+    let dlq:any[] = [];
+    for(let i=0;i<LLM_embedding.length;i++) {
+      dlq[i] = new sqs.Queue(this, 'DlqS3EventFifo'+i, {
+        visibilityTimeout: cdk.Duration.seconds(600),
+        queueName: `dlq-s3-event-for-${projectName}-${i}.fifo`,  
+        fifo: true,
+        contentBasedDeduplication: false,
+        deliveryDelay: cdk.Duration.millis(0),
+        retentionPeriod: cdk.Duration.days(14),
+      });
+    }
+
     // SQS for S3 event (fifo) 
     let queueUrl:string[] = [];
     let queue:any[] = [];
@@ -951,6 +964,10 @@ export class CdkKoreanChatbotStack extends cdk.Stack {
         contentBasedDeduplication: false,
         deliveryDelay: cdk.Duration.millis(0),
         retentionPeriod: cdk.Duration.days(2),
+        deadLetterQueue: {
+          maxReceiveCount: 4,
+          queue: dlq[i]
+        }
       });
       queueUrl.push(queue[i].queueUrl);
     }

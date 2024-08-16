38b9aef52a3ecc7ddf467c416366b27b77539a27
Kyoungsu Park <kyopark2014@gmail.com>
2024-08-16 10:25:51 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -91,6 +91,8 @@ token_counter_history = 0
 allowDualSearch = os.environ.get('allowDualSearch')
 allowDualSearchWithMulipleProcessing = True
 enableHybridSearch = os.environ.get('enableHybridSearch')
+useParrelWebSearch = True
+useEnhancedSearch = True
 
 minDocSimilarity = 200
 minCodeSimilarity = 300
@@ -3670,6 +3672,90 @@ def get_references_for_agent(docs):
 # define tools
 tools = [get_current_time, get_book_list, get_weather_info, search_by_tavily, search_by_opensearch]        
 
+def init_enhanced_search():
+    chat = get_chat() 
+
+    model = chat.bind_tools(tools)
+
+    class State(TypedDict):
+        messages: Annotated[list, add_messages]
+
+    tool_node = ToolNode(tools)
+
+    def should_continue(state: State) -> Literal["continue", "end"]:
+        messages = state["messages"]    
+        # print('(should_continue) messages: ', messages)
+            
+        last_message = messages[-1]
+        if not last_message.tool_calls:
+            return "end"
+        else:                
+            return "continue"
+
+    def call_model(state: State):
+        question = state["messages"]
+        print('question: ', question)
+            
+        if isKorean(question[0].content)==True:
+            system = (
+                "Assistant는 질문에 답변하기 위한 정보를 수집하는 연구원입니다."
+                "Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다."
+                "Assistant는 모르는 질문을 받으면 솔직히 모른다고 말합니다."
+                "최종 답변에는 조사한 내용을 반드시 포함하여야 하고, <result> tag를 붙여주세요."
+            )
+        else: 
+            system = (            
+                "You are a researcher charged with providing information that can be used when making answer."
+                "If you don't know the answer, just say that you don't know, don't try to make up an answer."
+                "You will be acting as a thoughtful advisor."
+                "Put it in <result> tags."
+            )
+                
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", system),
+                MessagesPlaceholder(variable_name="messages"),
+            ]
+        )
+        chain = prompt | model
+                
+        response = chain.invoke(question)
+        return {"messages": [response]}
+
+    def buildChatAgent():
+        workflow = StateGraph(State)
+
+        workflow.add_node("agent", call_model)
+        workflow.add_node("action", tool_node)
+            
+        workflow.set_entry_point("agent")
+        workflow.add_conditional_edges(
+            "agent",
+            should_continue,
+            {
+                "continue": "action",
+                "end": END,
+            },
+        )
+        workflow.add_edge("action", "agent")
+        return workflow.compile()
+    
+    return buildChatAgent()
+
+app_enhanced_search = init_enhanced_search()
+
+def enhanced_search(query):
+    inputs = [HumanMessage(content=query)]
+    config = {"recursion_limit": 50}
+        
+    result = app_enhanced_search.invoke({"messages": inputs}, config)   
+    print('result: ', result)
+            
+    message = result["messages"][-1]
+    print('enhanced_search: ', message)
+
+    return message.content[message.content.find('<result>')+8:len(message.content)-9]
+
 ####################### LangGraph #######################
 # Chat Agent Executor
 #########################################################
@@ -3873,6 +3959,155 @@ def run_reflection_agent(connectionId, requestId, query):
 
     return msg
 
+####################### LangGraph #######################
+# Knowledge Guru
+#########################################################
+
+def run_knowledge_guru(connectionId, requestId, query):
+    class State(TypedDict):
+        task: str
+        draft: str
+        critique: str
+        content: List[str]
+        revision_number: int
+        max_revisions: int
+            
+    def generation(state: State):    
+        draft = enhanced_search(state['task'])  
+        print('draft: ', draft)
+        
+        return {"draft": draft}
+    
+    class Queries(BaseModel):
+        """List of quries as a json format"""
+        queries: str = Field(description="queries relevant to the question'")
+    
+    def reflection(state: State):
+        system = """You are a researcher charged with providing information that can \
+    be used when writing the following answer. Generate a list of search queries that will gather \
+    any relevant information. Only generate 3 queries max. All queries should be words or string without numbers"""
+        
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", system),
+                ("human", "{answer}"),
+            ]
+        )
+            
+        chat = get_chat()
+        chain = prompt | chat
+        response = chain.invoke({"answer": state['draft']})
+        print('response: ', response.content)
+        
+        chat = get_chat()
+        structured_llm = chat.with_structured_output(Queries, include_raw=True)
+        info = structured_llm.invoke(response.content)
+        print('info: ', info)
+        
+        content = []
+        if not info['parsed'] == None:
+            queries = info['parsed']
+            print('queries: ', queries.queries)
+        
+            if useEnhancedSearch:
+                for q in json.loads(queries.queries):
+                    response = enhanced_search(q)     
+                    print(f'q: {q}, response: {response}')
+                    content.append(response)                   
+            else:
+                search = TavilySearchResults(k=2)
+                for q in json.loads(queries.queries):
+                    response = search.invoke(q)     
+                    for r in response:
+                        content.append(r['content'])    
+        return {
+            "content": content,
+            "draft": state["draft"]
+        }    
+        
+    def revise_answer(state: State):   
+        system = """Revise your previous answer using the new information as bellow. Then prvide the final answer with <result> tag.
+        
+        <information>
+        {content}
+        </information
+        """
+        
+        prompt = ChatPromptTemplate.from_messages(
+            [
+                ("system", system),
+                ("human", "<answer>{draft}</answer>"),
+            ]
+        )
+                
+        chat = get_chat()
+        chain = prompt | chat
+
+        response = chain.invoke({
+            "content": state['content'],
+            "draft": state['draft']
+        })
+        print('revise_answer: ', response.content)
+                
+        revision_number = state["revision_number"] if state.get("revision_number") is not None else 1
+        return {
+            "draft": response, 
+            "revision_number": revision_number + 1
+        }
+    
+    MAX_REVISIONS = 2
+    def should_continue(state: State, config):
+        max_revisions = config.get("configurable", {}).get("max_revisions", MAX_REVISIONS)
+        print("max_revisions: ", max_revisions)
+            
+        if state["revision_number"] > max_revisions:
+            return "end"
+        return "contine"
+
+    def buildKnowledgeGuru():    
+        workflow = StateGraph(State)
+
+        workflow.add_node("generation", generation)
+        workflow.add_node("reflection", reflection)
+        workflow.add_node("revise_answer", revise_answer)
+
+        workflow.set_entry_point("generation")
+
+        workflow.add_conditional_edges(
+            "revise_answer", 
+            should_continue, 
+            {
+                "end": END, 
+                "contine": "reflection"}
+        )
+
+        workflow.add_edge("generation", "reflection")
+        workflow.add_edge("reflection", "revise_answer")
+        
+        app = workflow.compile()
+        
+        return app
+    
+    app = buildKnowledgeGuru()
+        
+    isTyping(connectionId, requestId)    
+    inputs = {"task": query}
+    config = {
+        "recursion_limit": 50,
+        "max_revisions": 2
+    }
+    
+    for output in app.stream(inputs, config):   
+        for key, value in output.items():
+            print(f"Finished: {key}")
+            #print("value: ", value)
+            
+    print('value: ', value)
+        
+    readStreamMsg(connectionId, requestId, value["draft"].content)
+    
+    return value["draft"].content
+
 #########################################################
 def getResponse(connectionId, jsonBody):
     userId  = jsonBody['user_id']
@@ -4027,6 +4262,9 @@ def getResponse(connectionId, jsonBody):
                                                       
                 elif conv_type == 'agent-reflection':  # reflection
                     msg = run_reflection_agent(connectionId, requestId, text)     
+                    
+                elif conv_type == 'agent-knowledge-guru':  # knowledge guru
+                    msg = run_knowledge_guru(connectionId, requestId, text)      
                 
                 elif conv_type == 'qa':   # RAG
                     print(f'rag_type: {rag_type}')
